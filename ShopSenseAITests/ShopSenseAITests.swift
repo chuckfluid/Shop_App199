122395ca3d98645c820181004422c54d1e30ab2d
Chase <cfhotwing@gmail.com>
2025-07-27 05:51:06 -0400

    Initial Commit

---
+++ ShopSenseAITests/ShopSenseAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSenseAITests.swift
+//  ShopSenseAITests
+//
+//  Created by Chase Floied on 7/27/25.
+//
+
+import Testing
+@testable import ShopSenseAI
+
+struct ShopSenseAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
